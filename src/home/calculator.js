d841a327f8aec1e6abefbd498d64d7b4a34a557c
hamza8nawazi <barkatullahnawazih@gmail.com>
2024-11-07 19:13:31 +0500

    .

--- src/home/calculator.js
+++ src/home/calculator.js
@@ -4,29 +4,31 @@ import Button from "../components/Button/button";
 
 const Calculator = () => {
   const [input, setInput] = useState("");
-  const [result, setResult] = useState('');
+  const [result, setResult] = useState("");
 
- 
- const operators = ["+", "-", "*", "/"];
+  const operators = ["+", "-", "*", "/"];
 
- 
- const addToInput = (value) => {
-   if (operators.includes(input.slice(-1)) && operators.includes(value)) {
-     return;
-   }
-   setInput((prevInput) => prevInput + value);
- };
+  const addToInput = (value) => {
+    if (operators.includes(input.slice(-1)) && operators.includes(value)) {
+      return;
+    }
+    setInput((prevInput) => prevInput + value);
+  };
   const clearAll = () => {
     setInput("");
-    setResult('');
+    setResult("");
   };
 
   const calculateResult = () => {
-      const calculatedResult = eval(input);
-      setResult(calculatedResult);
-   
-  };
+    if (operators.includes(input.slice(-1))) {
+      alert("Error: Expression cannot end with an operator.");
+    setInput("error")
+      return;
+    }
 
+    const calculatedResult = eval(input);
+    setResult(calculatedResult);
+  };
 
   return (
     <div className="calculator">
@@ -36,25 +38,25 @@ const Calculator = () => {
         <div className="result">{result}</div>
       </div>
       <div className="buttons">
-      <Button value="1" onClick={addToInput} />
-      <Button value="2" onClick={addToInput} />
-      <Button value="3" onClick={addToInput} />
-      <Button value="+" onClick={addToInput} />
+        <Button value="1" onClick={addToInput} />
+        <Button value="2" onClick={addToInput} />
+        <Button value="3" onClick={addToInput} />
+        <Button value="+" onClick={addToInput} />
 
-      <Button value="4" onClick={addToInput} />
-      <Button value="5" onClick={addToInput} />
-      <Button value="6" onClick={addToInput} />
-      <Button value="-" onClick={addToInput} />
+        <Button value="4" onClick={addToInput} />
+        <Button value="5" onClick={addToInput} />
+        <Button value="6" onClick={addToInput} />
+        <Button value="-" onClick={addToInput} />
 
-      <Button value="7" onClick={addToInput} />
-      <Button value="8" onClick={addToInput} />
-      <Button value="9" onClick={addToInput} />
-      <Button value="*" onClick={addToInput} />
+        <Button value="7" onClick={addToInput} />
+        <Button value="8" onClick={addToInput} />
+        <Button value="9" onClick={addToInput} />
+        <Button value="*" onClick={addToInput} />
 
-      <Button value="C" onClick={clearAll} />
-      <Button value="0" onClick={addToInput} />
-      <Button value="=" onClick={calculateResult} />
-      <Button value="/" onClick={addToInput} />
+        <Button value="C" onClick={clearAll} />
+        <Button value="0" onClick={addToInput} />
+        <Button value="=" onClick={calculateResult} />
+        <Button value="/" onClick={addToInput} />
       </div>
     </div>
   );
